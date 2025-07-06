099048aa3ee2d15b419e31d07ee4ce3ee54ed46d
Chetan Manker <chetanmanker67@gmail.com>
2025-07-06 20:59:12 +0530

    v

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoute);
 app.use("/api/vapi", vapiRoutes);
 app.use("/api/database", databaseRoute);
-app.use("/user", scheduledInterview);
+app.use("/api/user", scheduledInterview);
 
 // Base route
 app.get("/", (req, res) => {
